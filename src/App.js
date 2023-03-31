e1018fa9a93c552abea23a2d8d8fbb2cd74feab0
VladyslavMazurets <vladmazurec@gmail.com>
2023-03-31 18:17:38 +0200

    change Navbar bg-color

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ function App() {
   return (
     <>
       <Navbar />
+      <p className="h-screen">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro ad odit
+        deleniti consectetur amet quod magni omnis possimus aperiam sint fuga,
+        illum consequatur, eius cupiditate.
+      </p>
+      <p className="h-screen">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro ad odit
+        deleniti consectetur amet quod magni omnis possimus aperiam sint fuga,
+        illum consequatur, eius cupiditate.
+      </p>
+      <p className="h-screen">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro ad odit
+        deleniti consectetur amet quod magni omnis possimus aperiam sint fuga,
+        illum consequatur, eius cupiditate.
+      </p>
+      <p className="h-screen">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro ad odit
+        deleniti consectetur amet quod magni omnis possimus aperiam sint fuga,
+        illum consequatur, eius cupiditate.
+      </p>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import TopBar from './TopBar';
+
+const NavBtn = 'transition delay-100 hover:text-orange-600';
 
 function Navbar() {
   return (
     <>
       <TopBar />
-      <div className="flex items-center w-full h-28 bg-orange-500 px-8">
-        <div className="flex justify-between w-full">
+      <div className="sticky top-0 flex items-center w-full h-28 bg-gray-200 px-8">
+        <div className="flex justify-between w-full text-black text-xl font-medium">
           <div>
-            <button type="button">Записатися на пробне заняття</button>
+            <button type="button" className={NavBtn}>
+              Записатися на пробне заняття
+            </button>
           </div>
 
-          <div>Title</div>
-          <div>
-            <button type="button">Про школу</button>
-            <button type="button">Курси</button>
-            <button type="button">Акції</button>
-            <button type="button">Галерея</button>
-            <button type="button">Контакти</button>
+          <div className="absolute left-[46%] top-[35%] text-3xl">
+            Zona Course
+          </div>
+
+          <div className="flex justify-between w-1/4">
+            <Link to="/" className={NavBtn}>
+              Про школу
+            </Link>
+            <Link to="/" className={NavBtn}>
+              Курси
+            </Link>
+            <button type="button" className={NavBtn}>
+              Акції
+            </button>
+            <Link to="/" className={NavBtn}>
+              Галерея
+            </Link>
+            <Link to="/" className={NavBtn}>
+              Контакти
+            </Link>
           </div>
         </div>
       </div>
